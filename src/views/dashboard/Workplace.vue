d206491ce5023c28bffb27ae46e352e3f90b4a35
jinyong <x-jinyong@hotmail.com>
2020-08-28 20:39:19 +0800

    first

---
+++ src/views/dashboard/Workplace.vue
@@ -0,0 +1,24 @@
+<template>
+  <page-view :title="false">
+    zxwh
+  </page-view>
+</template>
+
+<script>
+
+import { PageView } from '@/layouts'
+
+export default {
+  name: 'Workplace',
+  components: {
+    PageView
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
